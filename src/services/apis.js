05119387ab8593509ca94b7c4b9de549f950d02d
Shivam Kumar <141447573+Shivamgupta9170@users.noreply.github.com>
2025-01-21 13:16:05 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 // const BASE_URL = process.env.REACT_APP_BASE_URL;
 // const BASE_URL = "http://localhost:4000";
-const BASE_URL = "https://foodi-beige.vercel.app";
+const BASE_URL = "https://foodi-6n3n.onrender.com";
 
 // AUTH ENDPOINTS
 export const endpoints = {
